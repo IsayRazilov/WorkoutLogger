384fd0d51ba9022048793262169f6100dcaf9edb
Isay Razilov <isayrazilov@Isays-MacBook-Pro.local>
2018-12-14 21:51:27 +0200

    Added npm modules and connection to mongoose

---
+++ app.js
@@ -0,0 +1,39 @@
+var express      = require("express"),
+    mongoose     = require("mongoose"),
+    bodyParser   = require("body-parser");
+
+var app = express() ; 
+
+mongoose.connect("mongodb://localhost/workouts_logger");
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+
+var workoutSchema = new mongoose.Schema({
+    name: String, 
+    description: String
+});
+
+var Workout = mongoose.model("Workout", workoutSchema);
+
+var run = new Workout({
+    name: "Morning run",
+    description : "First run for the week"
+});
+
+run.save(function(err,workout){
+    if(err){
+        console.log("Error");
+    }else {
+        console.log("Added workout");
+        console.log(workout);
+    }
+})
+
+app.get("/home",function(req,res){
+    res.render("home");
+});
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+});
+
